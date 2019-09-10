639e8b60890faaf5ec7cb456ab252b92785552f1
adriens <Adrien.Sales@gmail.com>
2019-09-10 20:36:40 +1100

    Première version qui fonctionne

---
+++ src/main/java/com/adriens/colisnc/api/controller/ColisController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.adriens.colisnc.api.controller;
+
+import com.adriens.colisnc.api.service.ColisService;
+import com.adriens.github.colisnc.colisnc.ColisDataRow;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author 3004SAL
+ */
+@RestController
+public class ColisController {
+    @Autowired
+    private ColisService colisService;
+    
+    private final Logger log = LoggerFactory.getLogger(ColisController.class);
+    
+    @GetMapping("/colis/{itemId}")
+    public List<ColisDataRow> getColisRows(@PathVariable String itemId) throws Exception {
+        try{
+            return colisService.getColisRows(itemId);
+        }
+        catch(Exception ex){
+            log.error("Impossible de récupérer les détails du colis <"+ itemId + ">");
+            throw ex;
+        }
+    }
+
+}
---
+++ src/main/java/com/adriens/colisnc/api/service/ColisService.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.adriens.colisnc.api.service;
+
+import com.adriens.github.colisnc.colisnc.ColisCrawler;
+import com.adriens.github.colisnc.colisnc.ColisDataRow;
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author 3004SAL
+ */
+
+@Service
+public class ColisService {
+    private final Logger log = LoggerFactory.getLogger(ColisService.class);
+    //private ColisCrawler wrap;
+    
+    public List<ColisDataRow> getColisRows(String itemId) throws Exception {
+        ArrayList<ColisDataRow> out;
+        out = ColisCrawler.getColisRows(itemId);
+        return out;
+    }
+}
