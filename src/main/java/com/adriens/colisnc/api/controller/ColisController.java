95fb323a92aa93dc07c32182f792f62cd721f55b
adriens <Adrien.Sales@gmail.com>
2019-09-11 06:52:19 +1100

    Allow cross origins

--- src/main/java/com/adriens/colisnc/api/controller/ColisController.java
+++ src/main/java/com/adriens/colisnc/api/controller/ColisController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,7 @@ public class ColisController {
     private final Logger log = LoggerFactory.getLogger(ColisController.class);
     
     @GetMapping("/colis/{itemId}")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public List<ColisDataRow> getColisRows(@PathVariable String itemId) throws Exception {
         try{
             return colisService.getColisRows(itemId);
