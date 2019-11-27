233be0f0abc89e86e51539f35522ac8542c44294
ronny.soutart <soutartronny@gmail.com>
2019-11-27 12:27:00 +1100

    chore(colisController): Reorganize imports

--- src/main/java/com/adriens/colisnc/api/controller/ColisController.java
+++ src/main/java/com/adriens/colisnc/api/controller/ColisController.java
@@ -14,7 +14,12 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  *
